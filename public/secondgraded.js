b53b8a3ff8b86f0c4767c86fcb37e1377a8a8f29
Jairo <b2029jsoreolizo@pshs.edu.ph>
2025-08-11 16:13:40 +0800

    graded 2 3

---
+++ public/secondgraded.js
@@ -0,0 +1,6 @@
+function printMeal()
+{
+    totalOutput.innerHTML += in1.value + "<br>";
+    totalOutput.innerHTML += in2.value + "<br>";
+    totalOutput.innerHTML += in3.value + "<br>";
+}
---
+++ public/thirdgraded.js
@@ -0,0 +1,6 @@
+function calculateTotal()
+{
+    out1.innerHTML += in1.value + "<br>";
+    out1.innerHTML += in2.value + "<br>";
+    out1.innerHTML += in3.value + "<br>";
+}
